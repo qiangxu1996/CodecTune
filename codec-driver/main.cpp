b558d0e4cead497aad81e40ea2ccff7f6eca4925
Qiang Xu <qiangxu1996@gmail.com>
2021-04-03 22:53:31 -0400

    config, psnr/ssim, bitrate

--- codec-driver/main.cpp
+++ codec-driver/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <thread>
 #include <opencv2/videoio.hpp>
 #include <tbb/concurrent_queue.h>
@@ -5,13 +6,14 @@ extern "C" {
 #include <x265.h>
 }
 
+using std::string;
 using cv::Mat;
 
 class Encoder {
 public:
-    static x265_picture *PAUSE;
     static x265_picture *STOP;
 
+    using Config = std::vector<std::pair<string, string>>;
     using PicQueue = tbb::concurrent_bounded_queue<x265_picture *>;
     PicQueue loaded_pics;
     PicQueue free_pics;
@@ -22,12 +24,15 @@ public:
         param->sourceHeight = height;
         param->fpsNum = fps;
         param->fpsDenom = 1;
+        param->bEnablePsnr = 1;
+        param->bEnableSsim = 1;
+        param->rc.bitrate = 4500;
+        param->rc.rateControlMode = X265_RC_ABR;
 
         loaded_pics.set_capacity(QUEUE_SIZE);
         free_pics.set_capacity(QUEUE_SIZE);
         for (int i = 0; i < QUEUE_SIZE; i++) {
-            auto pic = x265_picture_alloc();
-            x265_picture_init(param, pic);
+            auto pic = new_pic();
             pic->stride[0] = width;
             pic->stride[1] = width;
             pic->stride[2] = width;
@@ -49,6 +54,7 @@ public:
 
     void operator()() {
         encoder = x265_encoder_open(param);
+        auto pic_out = new_pic();
         while (true) {
             x265_picture *pic;
             x265_nal *pp_nal;
@@ -58,26 +64,40 @@ public:
             if (pic == STOP) {
                 int ret;
                 do {
-                    ret = x265_encoder_encode(encoder, &pp_nal, &pi_nal, nullptr, nullptr);
+                    ret = x265_encoder_encode(encoder, &pp_nal, &pi_nal, nullptr, pic_out);
                 } while (ret > 0);
                 break;
             }
-            x265_encoder_encode(encoder, &pp_nal, &pi_nal, pic, nullptr);
+            x265_encoder_encode(encoder, &pp_nal, &pi_nal, pic, pic_out);
             delete[] static_cast<Mat *>(pic->userData);
             free_pics.push(pic);
         }
+        x265_picture_free(pic_out);
         x265_encoder_close(encoder);
+        encoder = nullptr;
+    }
+
+    void config(Config &config) {
+        for (auto &[k, v] : config)
+            x265_param_parse(param, k.c_str(), v.c_str());
+        if (encoder)
+            x265_encoder_reconfig(encoder, param);
     }
 
 private:
     const int QUEUE_SIZE = 24;
 
     x265_param *param = x265_param_alloc();
-    x265_encoder *encoder;
+    x265_encoder *encoder = nullptr;
+
+    x265_picture *new_pic() {
+        auto pic = x265_picture_alloc();
+        x265_picture_init(param, pic);
+        return pic;
+    }
 };
 
-auto Encoder::PAUSE = reinterpret_cast<x265_picture *>(1);
-auto Encoder::STOP = reinterpret_cast<x265_picture *>(2);
+auto Encoder::STOP = reinterpret_cast<x265_picture *>(1);
 
 int main(int argc, char *argv[]) {
     cv::VideoCapture video(argv[1]);
@@ -85,7 +105,14 @@ int main(int argc, char *argv[]) {
     int height = video.get(cv::CAP_PROP_FRAME_HEIGHT);
     double fps = video.get(cv::CAP_PROP_FPS);
 
+    std::ifstream config_file(argv[2]);
+    Encoder::Config config;
+    string k, v;
+    while (config_file >> k >> v)
+        config.push_back(std::make_pair(k, v));
+
     Encoder encoder(width, height, fps);
+    encoder.config(config);
     std::thread encoder_thread(std::ref(encoder));
 
     Mat frame;
